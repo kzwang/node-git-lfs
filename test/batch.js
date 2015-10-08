e8a96158b30be863d178eebc0423f92cce73ff8a
Kevin Wang <kevin807359@gmail.com>
2015-10-08 16:45:59 +1100

    add batch upload

--- test/batch.js
+++ test/batch.js
@@ -1,17 +1,61 @@
 var request = require('supertest');
+var config = require('config');
 
-var should = require('chai').should();
+var chai = require('chai');
+chai.use(require('chai-string'));
+
+var should = chai.should();
 
 var app = require('../lib/app');
 
+const BASE_URL = config.get('base_url');
+
 describe('Batch Endpoint', function() {
-    it('should not return 404', function(done) {
+    it('should return 422 for invalid request body', function(done) {
         request(app)
-            .post('/user/repo/objects/batch')
-            .end(function(err, res) {
-                should.not.exist(err);
-                res.status.should.not.equal(404);
-                done();
-            });
-    })
+            .post('/testuser/testrepo/objects/batch')
+            .send({})
+            .expect(422, done);
+    });
+
+    it('should return 422 for invalid operation', function(done) {
+        request(app)
+            .post('/testuser/testrepo/objects/batch')
+            .send( {
+                "operation": "invalid_operation",
+                "objects": [
+                    {
+                        "oid": "1111111",
+                        "size": 123
+                    }
+                ]
+            })
+            .expect(422, done);
+    });
+
+    it('should handle upload operation', function(done) {
+        request(app)
+            .post('/testuser/testrepo/objects/batch')
+            .send({
+                "operation": "upload",
+                "objects": [
+                    {
+                        "oid": "1111111",
+                        "size": 123
+                    }
+                ]
+            })
+            .expect(function(res) {
+                should.exist(res.body.objects);
+                res.body.objects.should.have.length(1);
+                res.body.objects[0].oid.should.equal('1111111');
+                res.body.objects[0].size.should.equal(123);
+
+                should.exist(res.body.objects[0].actions);
+                should.exist(res.body.objects[0].actions.upload);
+
+                res.body.objects[0].actions.upload.href.should.startWith(BASE_URL + '/testuser/testrepo/' );
+            })
+            .expect(200, done);
+    });
 });
