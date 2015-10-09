0e46c8baa6bee6140f3289ec938d181992df5194
Kevin Wang <kevin807359@gmail.com>
2015-10-09 23:11:37 +1100

    handle download operation

--- test/batch.js
+++ test/batch.js
@@ -60,8 +60,74 @@ describe('Batch Endpoint', function() {
                 should.exist(res.body.objects[0].actions);
                 should.exist(res.body.objects[0].actions.upload);
 
-                res.body.objects[0].actions.upload.href.should.startWith(BASE_URL + '/testuser/testrepo/objects/' );
+                res.body.objects[0].actions.upload.href.should.startWith(BASE_URL + 'testuser/testrepo/objects/' );
             })
             .expect(200, done);
+    });
+
+    it('should handle download non exist object', function(done) {
+        request(app)
+            .post('/testuser/testrepo/objects/batch')
+            .send({
+                "operation": "download",
+                "objects": [
+                    {
+                        "oid": "1111111",
+                        "size": 123
+                    }
+                ]
+            })
+            .expect(function(res) {
+                should.exist(res.body.objects);
+                res.body.objects.should.have.length(1);
+                res.body.objects[0].oid.should.equal('1111111');
+                res.body.objects[0].size.should.equal(123);
+
+                should.exist(res.body.objects[0].error);
+                res.body.objects[0].error.code.should.equal(404);
+            })
+            .expect(200, done);
+    });
+
+    it('should handle download operation', function(done) {
+        request(app)
+            .post('/testuser/testrepo/objects/batch')
+            .send({
+                "operation": "upload",
+                "objects": [
+                    {
+                        "oid": "1111111",
+                        "size": 123
+                    }
+                ]
+            })
+            .end(function(err, res) {
+                if (err) return done(err);
+                request(app)
+                    .post('/testuser/testrepo/objects/batch')
+                    .send({
+                        "operation": "download",
+                        "objects": [
+                            {
+                                "oid": "1111111",
+                                "size": 123
+                            }
+                        ]
+                    })
+                    .expect(function(res) {
+                        should.exist(res.body.objects);
+                        res.body.objects.should.have.length(1);
+                        res.body.objects[0].oid.should.equal('1111111');
+                        res.body.objects[0].size.should.equal(123);
+
+                        should.exist(res.body.objects[0].actions);
+                        should.exist(res.body.objects[0].actions.download);
+
+                        res.body.objects[0].actions.download.href.should.startWith(BASE_URL + 'testuser/testrepo/objects/' );
+                    })
+                    .expect(200, done);
+            });
+
+
     });
 });
